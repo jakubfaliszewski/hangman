a63ea01470f299d729b920c893f5a029e8d01851
Jakub Faliszewski <jfaliszewski@interia.pl>
2020-06-15 20:37:30 +0200

    basic UI/Main menu functionalities

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Hangman;
+using System;
 
 namespace hangman
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            UI.PrintTitle();
+            UI.PrintMainMenu();
+            GetMainMenu();
+        }
+
+        static void GetMainMenu()
+        {
+            switch (Console.ReadKey().Key.ToString())
+            {
+                case "D1":
+                    StandardGame();
+                    break;
+                case "D2":
+                    CustomGame();
+                    break;
+                case "D3":
+                    ExitGame();
+                    break;
+                default:
+                    Console.WriteLine(" - unknown input");
+                    Console.WriteLine("Please input valid key");
+                    Console.WriteLine("");
+                    GetMainMenu();
+                    break;
+            }
+        }
+
+        static void StandardGame()
+        {
+
+        }
+
+        static void CustomGame()
+        {
+
+        }
+
+        static void ExitGame()
+        {
+            Environment.Exit(0);
         }
     }
 }
---
+++ UI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman
+{
+    public class UI
+    {
+
+        public static void PrintTitle()
+        {
+            Console.WriteLine(@" _                                             ");
+            Console.WriteLine(@"| |                                            ");
+            Console.WriteLine(@"| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  ");
+            Console.WriteLine(@"| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ ");
+            Console.WriteLine(@"| | | | (_| | | | | (_| | | | | | | (_| | | | |");
+            Console.WriteLine(@"|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|");
+            Console.WriteLine(@"                    __/ |            ");
+            Console.WriteLine(@"                   |___/  ");
+            Console.WriteLine("");
+        }
+
+        public static void PrintMainMenu()
+        {
+            Console.WriteLine("1. Standard game (random word)");
+            Console.WriteLine("2. Custom game (custom word)");
+            Console.WriteLine("3. Exit");
+            //var userInput = Console.ReadKey().Key.ToString();
+
+            //return userInput;
+        }
+    }
+}
